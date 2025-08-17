34434bf4ed3a4d6edf1116b0f8c1e6d31ff0b603
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-08-17 03:45:24 -0500

    feat: :card_file_box: collection creations
    inscriptions collections insetions added
    
    none

---
+++ inscriptions.js
@@ -0,0 +1,138 @@
+db.inscripciones.insertMany([
+  {
+    "_id": "INS-001",
+    "usuario_id": "1023456789",
+    "curso_id": "CURS-007",
+    "fecha_inscripcion": "2025-01-15",
+    "estado": "activa",
+    "progreso": 40,
+    "calificacion": null
+  },
+  {
+    "_id": "INS-002",
+    "usuario_id": "4056789012",
+    "curso_id": "CURS-002",
+    "fecha_inscripcion": "2025-02-02",
+    "estado": "completada",
+    "progreso": 100,
+    "calificacion": 4.5
+  },
+  {
+    "_id": "INS-003",
+    "usuario_id": "1023456780",
+    "curso_id": "CURS-005",
+    "fecha_inscripcion": "2025-02-10",
+    "estado": "activa",
+    "progreso": 20,
+    "calificacion": null
+  },
+  {
+    "_id": "INS-004",
+    "usuario_id": "4056789012",
+    "curso_id": "CURS-007",
+    "fecha_inscripcion": "2025-03-01",
+    "estado": "pendiente",
+    "progreso": 0,
+    "calificacion": null
+  },
+  {
+    "_id": "INS-005",
+    "usuario_id": "1315678901",
+    "curso_id": "CURS-009",
+    "fecha_inscripcion": "2025-03-12",
+    "estado": "activa",
+    "progreso": 75,
+    "calificacion": null
+  },
+  {
+    "_id": "INS-006",
+    "usuario_id": "4056789012",
+    "curso_id": "CURS-010",
+    "fecha_inscripcion": "2025-03-20",
+    "estado": "completada",
+    "progreso": 100,
+    "calificacion": 4.9
+  },
+  {
+    "_id": "INS-007",
+    "usuario_id": "1416789012",
+    "curso_id": "CURS-007",
+    "fecha_inscripcion": "2025-04-05",
+    "estado": "activa",
+    "progreso": 60,
+    "calificacion": null
+  },
+  {
+    "_id": "INS-008",
+    "usuario_id": "1023456789",
+    "curso_id": "CURS-008",
+    "fecha_inscripcion": "2025-04-15",
+    "estado": "cancelada",
+    "progreso": 10,
+    "calificacion": null
+  },
+  {
+    "_id": "INS-009",
+    "usuario_id": "3045678901",
+    "curso_id": "CURS-006",
+    "fecha_inscripcion": "2025-04-25",
+    "estado": "activa",
+    "progreso": 50,
+    "calificacion": null
+  },
+  {
+    "_id": "INS-010",
+    "usuario_id": "4056789012",
+    "curso_id": "CURS-004",
+    "fecha_inscripcion": "2025-05-01",
+    "estado": "pendiente",
+    "progreso": 0,
+    "calificacion": null
+  },
+  {
+    "_id": "INS-011",
+    "usuario_id": "1214567890",
+    "curso_id": "CURS-001",
+    "fecha_inscripcion": "2025-05-10",
+    "estado": "activa",
+    "progreso": 85,
+    "calificacion": null
+  },
+  {
+    "_id": "INS-012",
+    "usuario_id": "1012345678",
+    "curso_id": "CURS-003",
+    "fecha_inscripcion": "2025-05-18",
+    "estado": "completada",
+    "progreso": 100,
+    "calificacion": 4.2
+  },
+  {
+    "_id": "INS-013",
+    "usuario_id": "1123456780",
+    "curso_id": "CURS-006",
+    "fecha_inscripcion": "2025-06-01",
+    "estado": "activa",
+    "progreso": 30,
+    "calificacion": null
+  },
+  {
+    "_id": "INS-014",
+    "usuario_id": "1134567891",
+    "curso_id": "CURS-009",
+    "fecha_inscripcion": "2025-06-05",
+    "estado": "cancelada",
+    "progreso": 15,
+    "calificacion": null
+  },
+  {
+    "_id": "INS-015",
+    "usuario_id": "8090123456",
+    "curso_id": "CURS-002",
+    "fecha_inscripcion": "2025-06-10",
+    "estado": "activa",
+    "progreso": 70,
+    "calificacion": null
+  }
+]
+)
